35824419f3573577bd2d38555b8847f26d457c8a
NtSuspendProcess <132966878+NtSuspendProcess@users.noreply.github.com>
2025-11-25 16:56:03 -0500

    Add files via upload

--- src/scripts/universal.lua
+++ src/scripts/universal.lua
@@ -1,1 +1,10 @@
+-- perish/src/scripts/universal.lua
+return function(env)
+	env.log("universal loaded")
 
+	local esp = import("lib/esp")()
+	esp.toggle(true)
+
+	env.undo.push(function() esp.toggle(false) end)
+end
+)
